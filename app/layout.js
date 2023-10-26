e1b2072baae0a8fcdd2e1745e2544d11a678c593
gulnoor <gulnoor5233@gmail.com>
2023-10-26 16:51:35 +0500

    added theme tokens

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={`dark ${inter.className}`}>
           <AppLayout>{children}</AppLayout>
         </body>
       </html>
